8221f70122bc23fee5192133410a8d3f473cc8b6
xu.junyang <xu.junyang@bestv.com.cn>
2016-09-25 21:12:19 +0800

    [reviewer] xu.junyang 1. relativeLayout

---
+++ LinearLayoutTest/app/src/main/java/com/example/linearlayouttest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.linearlayouttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by xujunyang on 16/9/25.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
