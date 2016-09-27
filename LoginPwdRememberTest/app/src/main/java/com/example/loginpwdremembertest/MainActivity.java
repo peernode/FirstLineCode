918dbeff27656e06b0c3cc87ce355826340dfc98
xu.junyang <xu.junyang@bestv.com.cn>
2016-09-27 22:30:30 +0800

    [reviewer] xu.junyang 1. LoginPwdRemember

---
+++ LoginPwdRememberTest/app/src/main/java/com/example/loginpwdremembertest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.loginpwdremembertest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by xujunyang on 16/9/27.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
