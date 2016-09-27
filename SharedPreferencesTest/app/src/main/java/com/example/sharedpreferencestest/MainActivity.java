da659622561c709b654a3abef5a42ea53e9a6713
xu.junyang <xu.junyang@bestv.com.cn>
2016-09-27 21:49:47 +0800

    [reviewer] xu.junyang 1. SharedPreferencesTest

---
+++ SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.sharedpreferencestest;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by xujunyang on 16/9/27.
+ */
+public class MainActivity extends Activity {
+    private Button saveData;
+    private Button restoreData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+
+        saveData = (Button)findViewById(R.id.save_data);
+        restoreData = (Button)findViewById(R.id.restore_data);
+
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("data",
+                        MODE_PRIVATE).edit();
+                editor.putString("name", "Tom");
+                editor.putInt("age", 28);
+                editor.putBoolean("married", false);
+                editor.commit();
+            }
+        });
+
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("data",
+                        MODE_PRIVATE);
+                String name = pref.getString("name","");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+
+                Toast.makeText(MainActivity.this, "name: "+name+" age: "+age+" married:"+married, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
